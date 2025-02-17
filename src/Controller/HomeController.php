93c419892e67eef6ced2bab123b29bb8bfd95d35
Albert Lecomte <albert.lecomte1989@gmail.com>
2025-02-17 19:36:40 +0100

    ADD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
   #[Route('/home', name: 'home')]
   public function home(): Response
   {
-    return $this->render('pages/home/index.html.twig', [
+    return $this->render('home/index.html.twig', [
       'controller_name' => 'HomeController',
     ]);
   }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,6 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,32 +21,73 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'attr'  => [
+                    'placeholder' => 'Pseudo',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un pseudo.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 100,
+                        'minMessage' => 'Le pseudo doit contenir au moins {{ limit }} lettres.',
+                        'maxMessage' => 'Le pseudo ne peut pas dépasser {{ limit }} lettres.',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Email',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une adresse email.',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter nos conditions.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe.',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'min' => 12,
+                        'max' => 255,
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le mot de passe ne peut pas dépasser {{ limit }} caractères',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/',
+                        'message' => 'Le mot de passe doit contenir au moins 12 caractères, dont au moins une majuscule, une minuscule, un chiffre et un caractère spécial.',
                     ]),
                 ],
+                'mapped' => false,
+                'first_options' => [
+                    'label' => ' Mot de passe ',
+                    'attr' => [
+                        'placeholder' => 'Votre mot de passe',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => [
+                        'placeholder' => 'mot de passe',
+                    ],
+                ],
+                'invalid_message' => 'Les mots de passe doivent correspondre.',
             ])
         ;
     }
